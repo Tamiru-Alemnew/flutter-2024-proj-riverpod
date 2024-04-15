30a0cfc5e06113ee034aa8491a60f1c06857b1a7
Tamiru Alemnew <tamirualemnew33@gmail.com>
2024-04-15 15:25:51 +0300

    create the parent screen

---
+++ family_cash_manager/android/app/src/main/kotlin/com/example/family_cash_manager/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.family_cash_manager
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ family_cash_manager/lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Family Cash manager',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Color.fromARGB(255, 78, 75, 82),
+          title: Text("Family Cash manager", selectionColor: Color(0xFFFFFFFF),),
+        ),
+
+        body: Container(
+          alignment: Alignment.center,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                alignment: Alignment.center,
+                height: double.tryParse("110"),
+                width: double.tryParse("140"),
+                color: const Color.fromARGB(255, 124, 122, 115),
+                child: Text("List of Expenses"),
+              ),
+              SizedBox(
+                height: double.tryParse("60"),
+              ),
+              Container(
+                alignment: Alignment.center,
+                height: double.tryParse("110"),
+                width: double.tryParse("140"),
+                color: Colors.green,
+                child: Text("Edit category"),
+                padding: EdgeInsets.all(12),
+              ),
+              SizedBox(
+                height: double.tryParse("60"),
+              ),
+              Container(
+                alignment: Alignment.center,
+                height: double.tryParse("110"),
+                width: double.tryParse("140"),
+                color: const Color.fromARGB(255, 85, 80, 80),
+                child: Text("Manage children"),
+                padding: EdgeInsets.all(12),
+              ),
+            ],
+          ),
+        ) ,
+      ),
+    );
+  }
+}
